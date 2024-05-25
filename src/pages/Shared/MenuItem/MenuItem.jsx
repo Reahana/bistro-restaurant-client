cd37f13c011bd9028f5fe0b8be9a9a888e98938e
Reahana <reahana233@gmail.com>
2024-05-25 20:16:46 +0600

    menu page

--- src/pages/Shared/MenuItem/MenuItem.jsx
+++ src/pages/Shared/MenuItem/MenuItem.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 const MenuItem = ({item}) => {
     const {name, image, price, recipe} = item;
+  
     return (
         <div className="flex space-x-2">
             <img style={{borderRadius: '0 200px 200px 200px'}} className="w-[100px]" src={image} alt="" />
