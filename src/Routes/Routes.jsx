8b4daa5173199ebb241f590867146d81d28512d8
Reahana <reahana233@gmail.com>
2024-05-26 11:46:13 +0600

    private route

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -7,6 +7,8 @@ import Menu from "../pages/Menu/Menu/Menu";
 import Order from "../pages/Order/Order/Order";
 import Login from "../pages/Login/Login";
 import SignUp from "../SignUp/SignUp";
+import PrivateRoute from "./PrivateRoute";
+import Secret from "../pages/Shared/Secret/Secret";
 
 const router = createBrowserRouter([
     {
@@ -33,6 +35,10 @@ const router = createBrowserRouter([
     path: 'signup',
     element: <SignUp></SignUp>
   },
+  {
+    path: 'secret',
+    element: <PrivateRoute><Secret></Secret></PrivateRoute>
+  }
       ]
     },
   ]);
