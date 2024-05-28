630e0143c66dfbcd99b1c93bc0c3c376f87f94ce
Reahana <reahana233@gmail.com>
2024-05-28 21:20:08 +0600

    image

--- src/pages/Dashboard/AddItems/AddItems.jsx
+++ src/pages/Dashboard/AddItems/AddItems.jsx
@@ -8,7 +8,7 @@ import Swal from "sweetalert2";
 
 
 const image_hosting_token = import.meta.env.VITE_IMAGE_UPLOAD_TOKEN;
-const image_hosting_url = `https://api.imgbb.com/1/upload?expiration=600&key=${image_hosting_token}`
+const image_hosting_url = `https://api.imgbb.com/1/upload?key=${image_hosting_token}`
 
 
 //const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
